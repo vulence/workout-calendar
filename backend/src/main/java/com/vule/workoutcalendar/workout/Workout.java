77970040c5a75a7032101e828653e56d96e69cfb
vulence <vukasinmarinkovic0@gmail.com>
2024-06-09 22:07:53 +0200

    added WorkoutExercise class tests

--- backend/src/main/java/com/vule/workoutcalendar/workout/Workout.java
+++ backend/src/main/java/com/vule/workoutcalendar/workout/Workout.java
@@ -45,6 +45,7 @@ public class Workout {
         if (rating < 0 || rating > 5) {
             throw new IllegalArgumentException("Rating must be between 0 and 5");
         }
+
         this.rating = rating;
     }
 
