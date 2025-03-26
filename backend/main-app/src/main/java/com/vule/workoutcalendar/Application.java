78dcf90f9e15fc84f3d1e8e8ad263b0a38d4073a
vulence <vukasinmarinkovic0@gmail.com>
2025-03-26 12:22:25 +0100

    added new modules to separate main app and security

--- backend/main-app/src/main/java/com/vule/workoutcalendar/Application.java
+++ backend/main-app/src/main/java/com/vule/workoutcalendar/Application.java
@@ -2,8 +2,9 @@ package com.vule.workoutcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.vule")
 public class Application {
 
 	public static void main(String[] args) {
