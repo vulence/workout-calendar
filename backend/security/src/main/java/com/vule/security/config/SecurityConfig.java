35d2b093ce81ba7b7bac8a997e3b79c1b059c8ca
vulence <vukasinmarinkovic0@gmail.com>
2025-03-26 12:22:25 +0100

    added new modules to separate main app and security

--- backend/security/src/main/java/com/vule/security/config/SecurityConfig.java
+++ backend/security/src/main/java/com/vule/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.vule.workoutcalendar.config;
+package com.vule.security.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
